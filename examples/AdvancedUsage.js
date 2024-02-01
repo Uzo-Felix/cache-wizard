ad10df2924a1bdab01dc49e08542d0a1e10469da
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2024-02-01 13:21:27 +0100

    Advanced usage example

---
+++ examples/AdvancedUsage.js
@@ -0,0 +1,26 @@
+const { CacheMiddleware } = require('../lib/CacheMiddleware');
+
+// Create a cache middleware instance with a maximum size of 1000 and hybrid options
+const cache = new CacheMiddleware(1000, { strategy: 'LRU' });
+
+// Set some key-value pairs in the cache
+cache.set('key1', 'value1');
+cache.set('key2', 'value2');
+cache.set('key3', 'value3');
+
+// Retrieve values from the cache
+console.log(cache.get('key1')); // Output: value1
+console.log(cache.get('key2')); // Output: value2
+console.log(cache.get('key3')); // Output: value3
+
+// Set the cache coherency strategy
+cache.setCoherencyStrategy('write-behind');
+
+// Invalidate a key in the cache
+cache.invalidate('key1');
+
+// Retrieve the invalidated key (should return null)
+console.log(cache.get('key1')); // Output: null
+
+// Clear the entire cache
+cache.clear();
